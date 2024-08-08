be768dc5cae9475833ab02f4496c44d6a3966eb9
zacharyad <zacharyadcoding@gmail.com>
2024-08-08 11:08:06 -0400

    testing branch rule

--- internal/templates/Index_templ.go
+++ internal/templates/Index_templ.go
@@ -60,7 +60,7 @@ func GuestIndex() templ.Component {
 			templ_7745c5c3_Var3 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div>Welcome guest</div>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div>Welcome guest!</div>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
